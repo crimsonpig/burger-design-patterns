bd930a596833530a4165f693322950dfe2ecd59c
crimsonpig <keith.stabbert@yahoo.com>
2015-04-21 21:24:40 -0700

    Adding cheese.

---
+++ In-N-Out-Burger/src/toppings/Cheese.cpp
@@ -0,0 +1,18 @@
+/*
+ * Cheese.cpp
+ *
+ *  Created on: Apr 21, 2015
+ *      Author: keith
+ */
+
+#include "Cheese.h"
+
+Cheese::Cheese(): Topping("cheese", "yellow cheese slices") {
+	// TODO Auto-generated constructor stub
+
+}
+
+Cheese::~Cheese() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ In-N-Out-Burger/src/toppings/Cheese.h
@@ -0,0 +1,19 @@
+/*
+ * Cheese.h
+ *
+ *  Created on: Apr 21, 2015
+ *      Author: keith
+ */
+
+#ifndef CHEESE_H_
+#define CHEESE_H_
+
+#include "Topping.h"
+
+class Cheese: public Topping {
+public:
+	Cheese();
+	~Cheese();
+};
+
+#endif /* CHEESE_H_ */
